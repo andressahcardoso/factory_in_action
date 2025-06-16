c5bc245f5e337a08abdc8ba8bc6cbbec9acdad82
Andressa <andressahenselcardoso@gmail.com>
2025-06-15 22:23:44 -0300

    Adicionando imagem de fundo e header

---
+++ script.js
@@ -0,0 +1,30 @@
+function atualizarDataHora() {
+  const now = new Date();
+
+  const dia = now.getDate().toString().padStart(2, '0');
+  const mes = (now.getMonth() + 1).toString().padStart(2, '0');
+  const ano = now.getFullYear();
+
+  const horas = now.getHours().toString().padStart(2, '0');
+  const minutos = now.getMinutes().toString().padStart(2, '0');
+  const segundos = now.getSeconds().toString().padStart(2, '0');
+
+  const dataHora = `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
+
+  document.getElementById('currentTime').textContent = dataHora;
+}
+
+atualizarDataHora();
+setInterval(atualizarDataHora, 1000);
+
+const btn = document.getElementById('btnFullscreen');
+
+btn.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch((err) => {
+        alert(`Erro ao entrar em tela cheia: ${err.message}`);
+    });
+    } else {
+        document.exitFullscreen();
+    }
+});
